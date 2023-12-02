67f99fc02603e0aed559d8741f9f403737c39b9d
p_kozhevnikov <p.kozhevnikov@cian.ru>
2023-12-02 07:51:35 +0300

    Added opportunity to set db path for tests

--- build/lib/python_tools/pytest_tools/conftest.py
+++ build/lib/python_tools/pytest_tools/conftest.py
@@ -6,25 +6,44 @@ from .tools import drop_tables, create_tables
 from typing import AsyncGenerator
 from faker import Faker
 import importlib
+from typing import TypedDict
+
+
+class EnvVariables(TypedDict):
+    db_schema_path: str
+    db_drop_schema_path: str
+    db_config_path: str
+
+
+@pytest.fixture(scope="session")
+def environ_variables() -> EnvVariables:
+    return EnvVariables(
+        db_schema_path='database/schema.sql',
+        db_drop_schema_path='database/drop_schema.sql',
+        db_config_path='database.db_config'
+    )
 
 
 @pytest.hookimpl(tryfirst=True)
-def pytest_sessionstart(session):
+def pytest_sessionstart(session, environ_variables: EnvVariables):
     os.environ['ENVIRON'] = 'test'
+    os.environ['DB_SCHEMA'] = environ_variables['db_schema_path']
+    os.environ['DB_DROP_SCHEMA'] = environ_variables['db_drop_schema_path']
+    os.environ['DB_CONFIG'] = environ_variables['db_config_path']
 
 
 @pytest_asyncio.fixture
 async def pg(pool: PostgresConnection) -> AsyncGenerator[PostgresConnection, None]:
-    await drop_tables(pool)
-    await create_tables(pool)
+    await drop_tables(pool, os.environ['DB_DROP_SCHEMA'])
+    await create_tables(pool, os.environ['DB_SCHEMA'])
     yield pool
-    await drop_tables(pool)
+    await drop_tables(pool, os.environ['DB_DROP_SCHEMA'])
     await pool._close_pool()
 
 
 @pytest_asyncio.fixture
 async def pool() -> PostgresConnection:
-    db_config = importlib.import_module('database.db_config')
+    db_config = importlib.import_module(os.environ['DB_CONFIG'])
     pg = db_config.pg
     await pg.create_pool()
     return pg.get()
